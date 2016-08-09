77b8cb2f7c4c208b46f9e5391ce3877e383ab365
clemence gillion <clemencegillion@gmail.com>
2016-08-09 17:09:03 +0200

    exercice done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/new'
+
+  # get 'articles/show'
+
+  # get 'articles/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  root to: 'articles#index'
 end
